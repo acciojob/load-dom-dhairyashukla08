45bac17270dd4ebab267b234543003e75a3d4e0d
Dhairya Shukla <113496448+dhairyashukla08@users.noreply.github.com>
2025-12-04 11:25:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function onload()
+{
+	const messageElement = document.createElement('p');
+	messageElement.textContent="DOM load success";
+	document.body.appendChild(messageElement);
+}
+document.addEventListener("DOMContentLoaded",onload);
